8c34b53082c5660577bfae63aa478230eb509f68
Brenda Long <brenda@studio6thave.com>
2021-09-07 09:48:50 -0500

    follow the chapters and code

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,4 +1,23 @@
+const loggedInUser = {
+	id: 1,
+	name: "Brenda",
+	email: "brenda.long@nashvillesoftwareschool.com",
+	dateJoined: 1630513631166,
+}
+
+export const getLoggedInUser = () => {
+	return loggedInUser;
+}
+
 export const getUsers = () => {
 	return fetch("http://localhost:8088/users")
 	.then(response => response.json())
+}
+
+export const getPosts = () => {
+	return fetch("http://localhost:8088/posts")
+	.then(response => {
+		console.log("what is response", response)
+		return response.json()
+	})
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,42 @@
-import { getUsers } from "./data/DataManager.js";
+// Can you explain what is being imported here?
+import { getPosts, getUsers } from "./data/DataManager.js";
+import { PostList } from "./feed/PostList.js";
+import { NavBar } from "./nav/NavBar.js";
 
-const allUsers = getUsers().then(apiUsers => {
-		console.log("now we can console the users", apiUsers)
+const showPostList = () => {
+	//Get a reference to the location on the DOM where the list will display
+	const postElement = document.querySelector(".postList");
+	getPosts().then((allPosts) => {
+		postElement.innerHTML = PostList(allPosts);
 	})
+}
 
-	
-console.log("LA LA LA allUsers", allUsers);
+const showNavBar = () => {
+    //Get a reference to the location on the DOM where the nav will display
+    const navElement = document.querySelector("nav");
+	navElement.innerHTML = NavBar();
+}
+
+const applicationElement = document.querySelector(".giffygram");
+
+// applicationElement.addEventListener("click", event => {
+// 	console.log("what was clicked", event)
+// 	if (event.target.id === "logout"){
+// 		console.log("You clicked on logout")
+// 	}
+// })
+
+const handleGiffyClick = (event) => {
+	console.log("what was clicked", event)
+	if (event.target.id === "logout"){
+		console.log("You clicked on logout")
+	}
+}
+applicationElement.addEventListener("click", handleGiffyClick)
+
+const startGiffyGram = () => {
+	showNavBar();
+	showPostList();
+}
+
+startGiffyGram();
